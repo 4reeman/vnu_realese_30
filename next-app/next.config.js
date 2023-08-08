5bd02454d33710c97c532bc6a2f86303edfb455f
freeman <4reeman>
2023-08-08 19:44:40 +0300

    next app migration to experimental dir

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  i18n: {
+    locales: ['en', 'de', 'fr'],
+    defaultLocale: 'en',
+    localeDetection: true,
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
